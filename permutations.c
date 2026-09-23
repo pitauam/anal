74d845646305372321fecd64e7ad99dbab07669f
pitauam <232541784+pitauam@users.noreply.github.com>
2026-09-23 18:33:16 +0200

    base

---
+++ permutations.c
@@ -0,0 +1,69 @@
+/**
+ *
+ * Descripcion: Implementation of function that generate permutations
+ *
+ * File: permutations.c
+ * Autor: Carlos Aguirre
+ * Version: 1.1
+ * Fecha: 21-09-2019
+ *
+ */
+
+
+#include "permutations.h"
+
+/***************************************************/
+/* Function: random_num Date:                      */
+/* Authors:                                        */
+/*                                                 */
+/* Rutine that generates a random number           */
+/* between two given numbers                       */
+/*                                                 */
+/* Input:                                          */
+/* int inf: lower limit                            */
+/* int sup: upper limit                            */
+/* Output:                                         */
+/* int: random number                              */
+/***************************************************/
+int random_num(int inf, int sup)
+{
+  /* your code */
+}
+
+/***************************************************/
+/* Function: generate_perm Date:                   */
+/* Authors:                                        */
+/*                                                 */
+/* Rutine that generates a random permutation      */
+/*                                                 */
+/* Input:                                          */
+/* int n: number of elements in the permutation    */
+/* Output:                                         */
+/* int *: pointer to integer array                 */
+/* that contains the permitation                   */
+/* or NULL in case of error                        */
+/***************************************************/
+int* generate_perm(int N)
+{
+  /* your code */
+}
+
+/***************************************************/
+/* Function: generate_permutations Date:           */
+/* Authors:                                        */
+/*                                                 */
+/* Function that generates n_perms random          */
+/* permutations with N elements                    */
+/*                                                 */
+/* Input:                                          */
+/* int n_perms: Number of permutations             */
+/* int N: Number of elements in each permutation   */
+/* Output:                                         */
+/* int**: Array of pointers to integer that point  */
+/* to each of the permutations                     */
+/* NULL en case of error                           */
+/***************************************************/
+int** generate_permutations(int n_perms, int N)
+{
+/* your code */
+}
---
+++ sorting.c
@@ -0,0 +1,38 @@
+/**
+ *
+ * Descripcion: Implementation of sorting functions
+ *
+ * Fichero: sorting.c
+ * Autor: Carlos Aguirre
+ * Version: 1.0
+ * Fecha: 16-09-2019
+ *
+ */
+
+
+#include "sorting.h"
+
+/***************************************************/
+/* Function: InsertSort    Date:                   */
+/* Your comment                                    */
+/***************************************************/
+int InsertSort(int* array, int ip, int iu)
+{
+  /* Your code */
+}
+
+
+/***************************************************/
+/* Function: SelectSort    Date:                   */
+/* Your comment                                    */
+/***************************************************/
+int BubbleSort(int* array, int ip, int iu)
+{
+  /* Your code */
+}
+
+
+
+
+
+
